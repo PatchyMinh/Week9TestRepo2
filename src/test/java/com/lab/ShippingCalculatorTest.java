6fdbf87e46542d785a6dda2e3329361ea26965d1
PatchyMinh <minhbuile1982@gmail.com>
2026-05-07 22:20:44 +0700

    Commit: tạo lỗi

--- src/test/java/com/lab/ShippingCalculatorTest.java
+++ src/test/java/com/lab/ShippingCalculatorTest.java
@@ -11,7 +11,7 @@ public class ShippingCalculatorTest {
     @Test
     void testExpress() {
         // FIX 4: 45000.0 thay vì 99999.0
-        assertEquals(45000.0, calc.calculate(5, "EXPRESS")); // 5 * 5000 + 20000 = 45000
+        assertEquals(99999.0, calc.calculate(5, "EXPRESS")); // 5 * 5000 + 20000 = 45000
     }
     @Test
     void testInvalidWeight() {
