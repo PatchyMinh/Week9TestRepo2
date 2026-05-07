38f7e3f3d973b8b3e493fac6549d3fde5f973189
PatchyMinh <minhbuile1982@gmail.com>
2026-05-07 22:18:43 +0700

    Commit: commit

--- src/test/java/com/lab/ShippingCalculatorTest.java
+++ src/test/java/com/lab/ShippingCalculatorTest.java
@@ -1,26 +1,21 @@
 package com.lab;
-
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-
 public class ShippingCalculatorTest {
-
     ShippingCalculator calc = new ShippingCalculator();
 
     @Test
     void testStandard() {
-        assertEquals(15000.0, calc.calculate(5, "STANDARD"));
+        assertEquals(15000.0, calc.calculate(5, "STANDARD")); // 5 * 3000 = 15000
     }
-
     @Test
     void testExpress() {
-        assertEquals(45000.0, calc.calculate(5, "EXPRESS"));
+        // FIX 4: 45000.0 thay vì 99999.0
+        assertEquals(45000.0, calc.calculate(5, "EXPRESS")); // 5 * 5000 + 20000 = 45000
     }
-
     @Test
     void testInvalidWeight() {
         assertThrows(IllegalArgumentException.class,
             () -> calc.calculate(-1, "STANDARD"));
     }
 }
-
